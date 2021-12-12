a6539c8f56019c73a6a039d3d2d1e25c46f7221a
raazeshp96 <raazeshp96@gmail.com>
2021-12-12 22:34:53 +0545

    fix:loader palacement

--- withPrivateRoute.tsx
+++ withPrivateRoute.tsx
@@ -2,8 +2,18 @@ import { Spin } from "antd";
 import { getAuth } from "firebase/auth";
 import router from "next/router";
 import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 import app from "./services/firebase";
 const auth = getAuth(app);
+
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    justify-content: center;
+`;
+
 const PrivateRoute = (AuthenticatedComponent: any) => {
     function PrivateComponent({ children }: any) {
         const [authUser, setauthUser] = useState<boolean>(false);
@@ -24,7 +34,13 @@ const PrivateRoute = (AuthenticatedComponent: any) => {
             });
         }, []);
 
-        return authUser ? <>{children}</> : <Spin></Spin>;
+        return authUser ? (
+            <>{children}</>
+        ) : (
+            <Wrapper>
+                <Spin tip="loading..." size="large" />
+            </Wrapper>
+        );
     }
     return class HOC extends React.Component {
         render() {
